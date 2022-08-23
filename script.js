d7356366411278c939a1a1bf1e67911772683493
DmitriyAsh <73988439+DmitriyAsh@users.noreply.github.com>
2022-08-23 20:08:09 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+'use strict';
+
+// 1)
+
+const tc = 10; // Пусть будет как пример
+const tf = (9/5) * tc + 32;
+alert(tf);
+
+// 2)
+
+let name = 'Василий';
+let admin = name; // Не понимаю почему let admin: void, но при этом все работает
+alert(admin);
+
+// 3)
+
+// 1000 + "108"= 1000108
